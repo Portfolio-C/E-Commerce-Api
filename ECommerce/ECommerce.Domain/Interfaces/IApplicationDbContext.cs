b8dcc8ea95ff7940f6f417001825d4b90f687e69
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-18 23:18:31 +0500

    update dbcontext and applicationuser

---
+++ ECommerce/ECommerce.Domain/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Domain.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<ApplicationUser> ApplicationsUsers { get; set; }
+    DbSet<Category> Categories { get; set; }
+    DbSet<Product> Products { get; set; }
+    DbSet<Attachment> Attachments { get; set; }
+
+    int SaveChanges();
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ ECommerce/ECommerce.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,67 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ECommerce.Infrastructure.Persistence;
+
+internal class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplicationDbContext
+{
+    public virtual DbSet<ApplicationUser> ApplicationsUsers { get; set; }
+    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<Attachment> Attachments { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+
+        #region Identity
+
+        builder.Entity<ApplicationUser>(e =>
+        {
+            e.ToTable("User");
+        });
+
+        builder.Entity<IdentityRole>(e =>
+        {
+            e.ToTable("Role");
+        });
+
+        builder.Entity<IdentityUserClaim<string>>(e =>
+        {
+            e.ToTable("UserClaim");
+        });
+
+        builder.Entity<IdentityUserLogin<string>>(e =>
+        {
+            e.ToTable("UserLogin");
+        });
+
+        builder.Entity<IdentityUserToken<string>>(e =>
+        {
+            e.ToTable("UserToken");
+        });
+
+        builder.Entity<IdentityRoleClaim<string>>(e =>
+        {
+            e.ToTable("RoleClaim");
+        });
+
+        builder.Entity<IdentityUserRole<string>>(e =>
+        {
+            e.ToTable("UserRole");
+        });
+
+        #endregion
+    }
+}
