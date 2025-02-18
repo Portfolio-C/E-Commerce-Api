ffc321c68f3266c14de08e48dc3450e32e1eb868
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-18 23:19:04 +0500

    added configurations for every entity

---
+++ ECommerce/ECommerce.Infrastructure/Persistence/Configurations/AttachmentConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Infrastructure.Persistence.Configurations;
+
+internal sealed class AttachmentConfiguration : IEntityTypeConfiguration<Attachment>
+{
+    public void Configure(EntityTypeBuilder<Attachment> builder)
+    {
+        builder.ToTable("Attachments");
+
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.FileName)
+            .IsRequired()
+            .HasMaxLength(Constants.DEFAULT_STRING_LENGTH);
+
+        builder.Property(a => a.FileType)
+            .IsRequired();
+
+        builder.Property(a => a.FileData)
+            .IsRequired();
+
+        builder.HasOne(a => a.Product)
+            .WithMany(p => p.Attachments)
+            .HasForeignKey(a => a.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
