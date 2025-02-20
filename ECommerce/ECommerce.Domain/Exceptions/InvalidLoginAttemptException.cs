b43450b23b6d3ef66b6765bdf619a3b1411a7401
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-20 21:15:04 +0500

    added some custom exceptions

---
+++ ECommerce/ECommerce.Domain/Exceptions/InvalidLoginAttemptException.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Domain.Exceptions;
+
+public class InvalidLoginAttemptException : Exception
+{
+    public InvalidLoginAttemptException() : base() { }
+    public InvalidLoginAttemptException(string message) : base(message) { }
+    public InvalidLoginAttemptException(string message, Exception innerException) : base(message, innerException) { }
+}
