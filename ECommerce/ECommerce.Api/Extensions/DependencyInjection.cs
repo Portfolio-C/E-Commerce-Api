11be42814a5e5ef6c8786508aacae13e52b1073d
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-20 21:13:16 +0500

    updated dependency injection

--- ECommerce/ECommerce.Api/Extensions/DependencyInjection.cs
+++ ECommerce/ECommerce.Api/Extensions/DependencyInjection.cs
@@ -1,5 +1,13 @@
-﻿using ECommerce.Application.Extensions;
+﻿using ECommerce.Application.Configurations;
+using ECommerce.Application.Extensions;
 using ECommerce.Infrastructure.Extensions;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Reflection;
 
 namespace ECommerce.Api.Extensions;
 
@@ -10,6 +18,107 @@ public static class DependencyInjection
         services.RegisterApplication();
         services.RegisterInfrastructure(configuration);
 
+        AddControllers(services);
+        AddAuthentication(services, configuration);
+        AddConfigurations(services, configuration);
+        AddSwagger(services);
+
         return services;
+    }
+
+    private static void AddControllers(IServiceCollection services)
+    {
+        services.AddControllers(options =>
+        {
+            options.SuppressAsyncSuffixInActionNames = false;
+            options.ReturnHttpNotAcceptable = true;
+        })
+            .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.Converters.Add(new StringEnumConverter());
+            })
+            .AddXmlSerializerFormatters();
+    }
+
+    private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtOptions = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>()
+            ?? throw new InvalidOperationException("Could not load JWT configurations.");
+
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtOptions.SecretKey))
+                };
+            });
+    }
+
+    private static void AddConfigurations(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<JwtOptions>()
+            .Bind(configuration.GetSection(JwtOptions.SectionName))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+    }
+
+    private static void AddSwagger(IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "E-Commerce API",
+                Description = "E-Commerce REST API",
+                Contact = new OpenApiContact
+                {
+                    Name = "Choriyev Ramazon",
+                    Email = "shukhratovich75@gmail.com",
+                    Url = new Uri("https://e-commerce.uz"),
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                BearerFormat = "JWT",
+                Name = "JWT Authentication",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                Description = "Enter your JWT token in the text input below.",
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                { securityScheme, [] }
+            });
+        });
     }
 }
