e211bfd571051f03e943c59a0cccf52a6b3b9fda
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-20 21:12:15 +0500

    created jwt options

---
+++ ECommerce/ECommerce.Application/Configurations/JwtOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Application.Configurations;
+
+public class JwtOptions
+{
+    public const string SectionName = nameof(JwtOptions);
+
+    [Required(ErrorMessage = "Secret Key is required.")]
+    public required string SecretKey { get; init; }
+
+    [Required(ErrorMessage = "Expiration time is required.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Expiration time must be greater than 1 hour.")]
+    public int ExpiresInHours { get; init; }
+}
