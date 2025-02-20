856a65d0300747519b9d9a514ef24136c2b025ca
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-20 21:16:41 +0500

    token handler was created for generating refresh and access tokens

---
+++ ECommerce/ECommerce.Application/Interfaces/ITokenHandler.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Domain.Entities;
+
+namespace ECommerce.Application.Interfaces;
+
+public interface ITokenHandler
+{
+    string GenerateAccessToken(ApplicationUser user, IList<string> roles);
+    string GenerateRefreshToken();
+}
