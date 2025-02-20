f4729a508155d45ce9b4eb51abaeec44e3f7b9cc
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-20 21:14:40 +0500

    created refresh token entity

---
+++ ECommerce/ECommerce.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Domain.Common;
+
+namespace ECommerce.Domain.Entities;
+
+public class RefreshToken : EntityBase
+{
+    public required string Token { get; set; }
+    public DateTime ExpiresAtUtc { get; set; }
+    public bool IsRevoked { get; set; }
+
+    public required string UserId { get; set; }
+    public required ApplicationUser User { get; set; }
+}
---
+++ ECommerce/ECommerce.Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Infrastructure.Persistence.Configurations;
+
+internal sealed class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.ToTable("RefreshTokens");
+
+        builder.HasKey(r => r.Id);
+
+        builder
+            .HasIndex(x => x.Token)
+            .IsUnique();
+
+        builder
+            .HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .IsRequired();
+
+        builder
+            .Navigation(x => x.User)
+            .AutoInclude();
+    }
+}
