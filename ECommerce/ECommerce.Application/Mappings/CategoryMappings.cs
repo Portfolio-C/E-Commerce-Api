7cac83a2b701f96ca1267fbce301c8359c4d3a00
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-23 12:22:30 +0500

    add automapper

---
+++ ECommerce/ECommerce.Application/Mappings/CategoryMappings.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ECommerce.Application.DTOs.Category;
+using ECommerce.Application.Requests.Category;
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Application.Mappings;
+
+public class CategoryMappings : Profile
+{
+    public CategoryMappings()
+    {
+        CreateMap<Category, CategoryDto>();
+        CreateMap<CreateCategoryRequest, Category>();
+        CreateMap<UpdateCategoryRequest, Category>();
+    }
+}
