c8683ee5975908c82fa64c6edfbba064c6f8f7a2
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-23 12:21:23 +0500

    add category service

---
+++ ECommerce/ECommerce.Application/Interfaces/ICateogryService.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Application.DTOs.Category;
+using ECommerce.Application.Requests.Category;
+
+namespace ECommerce.Application.Interfaces;
+
+public interface ICateogryService
+{
+    Task<List<CategoryDto>> GetAsync();
+    Task<CategoryDto> GetByIdAsync(int id);
+    Task<CategoryDto> CreateAsync(CreateCategoryRequest request);
+    Task<CategoryDto> UpdateAsync(UpdateCategoryRequest request);
+    Task DeleteAsync(int id);
+}
